f820ad5b96758f743f6b50dfa8ab8c61a1bab787
Lennart Becker <lennart.becker@d-fine.com>
2025-05-15 14:18:17 +0200

    Update logging UATTRIBUTE_VERSION

--- src/ZenohUTransport.cpp
+++ src/ZenohUTransport.cpp
@@ -103,7 +103,7 @@ ZenohUTransport::uattributesToAttachment(const v1::UAttributes& attributes) {
 
 	std::vector<uint8_t> version = {UATTRIBUTE_VERSION};
 
-	spdlog::debug("uattributesToAttachement UAttribute version: {}; constexpr UATTRIBUTE_VERSION: {}", static_cast<int>(version[0]), UATTRIBUTE_VERSION);
+	spdlog::debug("uattributesToAttachement UAttribute version: {}", static_cast<int>(version[0]));
 
 
 	std::vector<uint8_t> data(attributes.ByteSizeLong());
